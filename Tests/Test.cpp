a70ce187657dce9d18d7de491b0de438c1eb34f0
isaaklindgren <isali@student.chalmers.se>
2021-02-08 13:49:05 +0100

    Initial try to make gtest and gmock work

---
+++ Tests/Test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by isaaklindgren on 2021-02-08.
+//
+
+
+
+#include "gtest/gtest.h"
+
+TEST(Test,test){
+    ASSERT_TRUE(1==1);
+}
+
+TEST(TESTSUITETWO,TESTONE){
+    ASSERT_TRUE(1!=2);
+}
