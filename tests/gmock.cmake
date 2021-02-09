3767c7a3709e3f2b7fa72b44f228ede19611abe1
isaaklindgren <isali@student.chalmers.se>
2021-02-09 11:44:28 +0100

    travis test

--- tests/gmock.cmake
+++ tests/gmock.cmake
@@ -1,15 +1,15 @@
 set(GOOGLEMOCK_ROOT gtest/googlemock CACHE STRING "Google Mock source root")
 
 include_directories(SYSTEM
-       # ../${GOOGLEMOCK_ROOT}
-       # ../${GOOGLEMOCK_ROOT}/include
+        ../${GOOGLEMOCK_ROOT}
+        ../${GOOGLEMOCK_ROOT}/include
         ${CMAKE_SOURCE_DIR}/${GOOGLEMOCK_ROOT}
         ${CMAKE_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/include
         )
 
 set(GOOGLEMOCK_SOURCES
-     #   ../${GOOGLEMOCK_ROOT}/src/gmock-all.cc
-     #   ../${GOOGLEMOCK_ROOT}/src/gmock_main.cc
+        ../${GOOGLEMOCK_ROOT}/src/gmock-all.cc
+        ../${GOOGLEMOCK_ROOT}/src/gmock_main.cc
         ${CMAKE_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock-all.cc
         ${CMAKE_SOURCE_DIR}/${GOOGLEMOCK_ROOT}/src/gmock_main.cc
         )
