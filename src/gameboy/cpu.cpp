2215d0d7ead2e2c89b7e02cd23c19bdeeae9ee4b
isaaklindgren <isali@student.chalmers.se>
2021-02-23 15:11:48 +0100

    set and reset

--- src/gameboy/cpu.cpp
+++ src/gameboy/cpu.cpp
@@ -402,9 +402,22 @@ void CPU::reset(uint8_t nth_byte) {
  * @param value the value of the byte which should be used
  */
 void CPU::bit(uint8_t bit_nr, uint8_t value) {
-    F.z  = (~value >> bit_nr) & 0x01;
+    F.z = (~value >> bit_nr) & 0x01;
     F.n = 0;
     F.h = 1;
+}
+/**
+ * Reset bit_nr in reg.
+ * * */
+void res(uint8_t bit_nr, uint8_t &reg) {
+    reg = reg & (0x01 << bit_nr);
+}
+
+/**
+ * Set bit_nr in reg.
+ * * */
+void set(uint8_t bit_nr,uint8_t &reg){
+    reg = reg | (0x01 << bit_nr);
 }
 /**
  * Every time we read PC, we want to increment it.
@@ -1077,7 +1090,7 @@ void CPU::execute_instruction() {
             subA(read_and_inc_pc(), true);
             break;
         case 0xDF:
-            reset(3);
+            reset(2);
             break;
         case 0xE1:
             popReg(HL);
@@ -1089,7 +1102,7 @@ void CPU::execute_instruction() {
             andA(read_and_inc_pc());
             break;
         case 0xE7:
-            reset(4);
+            reset(3);
             break;
         case 0xE9:
             jump(HL.all_16);
@@ -1147,7 +1160,7 @@ int CPU::CB_ops() {
         case 0x05:
             rlc(HL.low_8);
             return 2;
-        //TODO 0X06
+            //TODO 0X06
         case 0x07:
             rlc(A.high_8);
             return 2;
@@ -1221,197 +1234,554 @@ int CPU::CB_ops() {
             return 2;
 
         case 0x40:
-            bit(0,BC.high_8);
+            bit(0, BC.high_8);
             return 2;
         case 0x41:
-            bit(0,BC.low_8);
+            bit(0, BC.low_8);
             return 2;
         case 0x42:
-            bit(0,DE.high_8);
+            bit(0, DE.high_8);
             return 2;
         case 0x43:
-            bit(0,DE.low_8);
+            bit(0, DE.low_8);
             return 2;
         case 0x44:
-            bit(0,HL.high_8);
+            bit(0, HL.high_8);
             return 2;
         case 0x45:
-            bit(0,HL.low_8);
+            bit(0, HL.low_8);
             return 2;
         case 0x46:
-            bit(0,memory->read(HL.all_16));
+            bit(0, memory->read(HL.all_16));
             return 4;
         case 0x47:
-            bit(0,A.high_8);
+            bit(0, A.high_8);
             return 2;
         case 0x48:
-            bit(1,BC.high_8);
+            bit(1, BC.high_8);
             return 2;
         case 0x49:
-            bit(1,BC.low_8);
+            bit(1, BC.low_8);
             return 2;
         case 0x4A:
-            bit(1,DE.high_8);
+            bit(1, DE.high_8);
             return 2;
         case 0x4B:
-            bit(1,DE.low_8);
+            bit(1, DE.low_8);
             return 2;
         case 0x4C:
-            bit(1,HL.high_8);
+            bit(1, HL.high_8);
             return 2;
         case 0x4D:
-            bit(1,HL.low_8);
+            bit(1, HL.low_8);
             return 2;
         case 0x4E:
-            bit(1,memory->read(HL.all_16));
+            bit(1, memory->read(HL.all_16));
             return 4;
         case 0x4F:
-            bit(1,A.high_8);
+            bit(1, A.high_8);
             return 2;
         case 0x50:
-            bit(2,BC.high_8);
+            bit(2, BC.high_8);
             return 2;
         case 0x51:
-            bit(2,BC.low_8);
+            bit(2, BC.low_8);
             return 2;
         case 0x52:
-            bit(2,DE.high_8);
+            bit(2, DE.high_8);
             return 2;
         case 0x53:
-            bit(2,DE.low_8);
+            bit(2, DE.low_8);
             return 2;
         case 0x54:
-            bit(2,HL.high_8);
+            bit(2, HL.high_8);
             return 2;
         case 0x55:
-            bit(2,HL.low_8);
+            bit(2, HL.low_8);
             return 2;
         case 0x56:
-            bit(2,memory->read(HL.all_16));
+            bit(2, memory->read(HL.all_16));
             return 4;
         case 0x57:
-            bit(2,A.high_8);
+            bit(2, A.high_8);
             return 2;
         case 0x58:
-            bit(3,BC.high_8);
+            bit(3, BC.high_8);
             return 2;
         case 0x59:
-            bit(3,BC.low_8);
+            bit(3, BC.low_8);
             return 2;
         case 0x5A:
-            bit(3,DE.high_8);
+            bit(3, DE.high_8);
             return 2;
         case 0x5B:
-            bit(3,DE.low_8);
+            bit(3, DE.low_8);
             return 2;
         case 0x5C:
-            bit(3,HL.high_8);
+            bit(3, HL.high_8);
             return 2;
         case 0x5D:
-            bit(3,HL.low_8);
+            bit(3, HL.low_8);
             return 2;
         case 0x5E:
-            bit(3,memory->read(HL.all_16));
+            bit(3, memory->read(HL.all_16));
             return 4;
         case 0x5F:
-            bit(3,A.high_8);
+            bit(3, A.high_8);
             return 2;
         case 0x60:
-            bit(4,BC.high_8);
+            bit(4, BC.high_8);
             return 2;
         case 0x61:
-            bit(4,BC.low_8);
+            bit(4, BC.low_8);
             return 2;
         case 0x62:
-            bit(4,DE.high_8);
+            bit(4, DE.high_8);
             return 2;
         case 0x63:
-            bit(4,DE.low_8);
+            bit(4, DE.low_8);
             return 2;
         case 0x64:
-            bit(4,HL.high_8);
+            bit(4, HL.high_8);
             return 2;
         case 0x65:
-            bit(4,HL.low_8);
+            bit(4, HL.low_8);
             return 2;
         case 0x66:
-            bit(4,memory->read(HL.all_16));
+            bit(4, memory->read(HL.all_16));
             return 4;
         case 0x67:
-            bit(4,A.high_8);
+            bit(4, A.high_8);
             return 2;
         case 0x68:
-            bit(5,BC.high_8);
+            bit(5, BC.high_8);
             return 2;
         case 0x69:
-            bit(5,BC.low_8);
+            bit(5, BC.low_8);
             return 2;
         case 0x6A:
-            bit(5,DE.high_8);
+            bit(5, DE.high_8);
             return 2;
         case 0x6B:
-            bit(5,DE.low_8);
+            bit(5, DE.low_8);
             return 2;
         case 0x6C:
-            bit(5,HL.high_8);
+            bit(5, HL.high_8);
             return 2;
         case 0x6D:
-            bit(5,HL.low_8);
+            bit(5, HL.low_8);
             return 2;
         case 0x6E:
-            bit(5,memory->read(HL.all_16));
+            bit(5, memory->read(HL.all_16));
             return 4;
         case 0x6F:
-            bit(5,A.high_8);
+            bit(5, A.high_8);
             return 2;
         case 0x70:
-            bit(6,BC.high_8);
+            bit(6, BC.high_8);
             return 2;
         case 0x71:
-            bit(6,BC.low_8);
+            bit(6, BC.low_8);
             return 2;
         case 0x72:
-            bit(6,DE.high_8);
+            bit(6, DE.high_8);
             return 2;
         case 0x73:
-            bit(6,DE.low_8);
+            bit(6, DE.low_8);
             return 2;
         case 0x74:
-            bit(6,HL.high_8);
+            bit(6, HL.high_8);
             return 2;
         case 0x75:
-            bit(6,HL.low_8);
+            bit(6, HL.low_8);
             return 2;
         case 0x76:
-            bit(6,memory->read(HL.all_16));
+            bit(6, memory->read(HL.all_16));
             return 4;
         case 0x77:
-            bit(6,A.high_8);
+            bit(6, A.high_8);
             return 2;
         case 0x78:
-            bit(7,BC.high_8);
+            bit(7, BC.high_8);
             return 2;
         case 0x79:
-            bit(7,BC.low_8);
+            bit(7, BC.low_8);
             return 2;
         case 0x7A:
-            bit(7,DE.high_8);
+            bit(7, DE.high_8);
             return 2;
         case 0x7B:
-            bit(7,DE.low_8);
+            bit(7, DE.low_8);
             return 2;
         case 0x7C:
-            bit(7,HL.high_8);
+            bit(7, HL.high_8);
             return 2;
         case 0x7D:
-            bit(7,HL.low_8);
+            bit(7, HL.low_8);
             return 2;
         case 0x7E:
-            bit(7,memory->read(HL.all_16));
+            bit(7, memory->read(HL.all_16));
             return 4;
         case 0x7F:
-            bit(7,A.high_8);
+            bit(7, A.high_8);
             return 2;
+
+        case 0x80:
+            res(0, BC.high_8);
+            return 2;
+        case 0x81:
+            res(0, BC.low_8);
+            return 2;
+        case 0x82:
+            res(0, DE.high_8);
+            return 2;
+        case 0x83:
+            res(0, DE.low_8);
+            return 2;
+        case 0x84:
+            res(0, HL.high_8);
+            return 2;
+        case 0x85:
+            res(0, HL.low_8);
+            return 2;
+     //TODO 0x86
+        case 0x87:
+            res(0, A.high_8);
+            return 2;
+        case 0x88:
+            res(1, BC.high_8);
+            return 2;
+        case 0x89:
+            res(1, BC.low_8);
+            return 2;
+        case 0x8A:
+            res(1, DE.high_8);
+            return 2;
+        case 0x8B:
+            res(1, DE.low_8);
+            return 2;
+        case 0x8C:
+            res(1, HL.high_8);
+            return 2;
+        case 0x8D:
+            res(1, HL.low_8);
+            return 2;
+        //TODO 0X8E
+        case 0x8F:
+            res(1, A.high_8);
+            return 2;
+
+        case 0x90:
+            res(2, BC.high_8);
+            return 2;
+        case 0x91:
+            res(2, BC.low_8);
+            return 2;
+        case 0x92:
+            res(2, DE.high_8);
+            return 2;
+        case 0x93:
+            res(2, DE.low_8);
+            return 2;
+        case 0x94:
+            res(2, HL.high_8);
+            return 2;
+        case 0x95:
+            res(2, HL.low_8);
+            return 2;
+            //TODO 0x96
+        case 0x97:
+            res(2, A.high_8);
+            return 2;
+        case 0x98:
+            res(3, BC.high_8);
+            return 2;
+        case 0x99:
+            res(3, BC.low_8);
+            return 2;
+        case 0x9A:
+            res(3, DE.high_8);
+            return 2;
+        case 0x9B:
+            res(3, DE.low_8);
+            return 2;
+        case 0x9C:
+            res(3, HL.high_8);
+            return 2;
+        case 0x9D:
+            res(3, HL.low_8);
+            return 2;
+            //TODO 0X9E
+        case 0x9F:
+            res(3, A.high_8);
+            return 2;
+        case 0xA0:
+            res(4, BC.high_8);
+            return 2;
+        case 0xA1:
+            res(4, BC.low_8);
+            return 2;
+        case 0xA2:
+            res(4, DE.high_8);
+            return 2;
+        case 0xA3:
+            res(4, DE.low_8);
+            return 2;
+        case 0xA4:
+            res(4, HL.high_8);
+            return 2;
+        case 0xA5:
+            res(4, HL.low_8);
+            return 2;
+            //TODO 0x96
+        case 0xA7:
+            res(4, A.high_8);
+            return 2;
+        case 0xA8:
+            res(5, BC.high_8);
+            return 2;
+        case 0xA9:
+            res(5, BC.low_8);
+            return 2;
+        case 0xAA:
+            res(5, DE.high_8);
+            return 2;
+        case 0xAB:
+            res(5, DE.low_8);
+            return 2;
+        case 0xAC:
+            res(5, HL.high_8);
+            return 2;
+        case 0xAD:
+            res(5, HL.low_8);
+            return 2;
+            //TODO 0XAE
+        case 0xAF:
+            res(5, A.high_8);
+            return 2;
+        case 0xB0:
+            res(6, BC.high_8);
+            return 2;
+        case 0xB1:
+            res(6, BC.low_8);
+            return 2;
+        case 0xB2:
+            res(6, DE.high_8);
+            return 2;
+        case 0xB3:
+            res(6, DE.low_8);
+            return 2;
+        case 0xB4:
+            res(6, HL.high_8);
+            return 2;
+        case 0xB5:
+            res(6, HL.low_8);
+            return 2;
+            //TODO 0xB6
+        case 0xB7:
+            res(6, A.high_8);
+            return 2;
+        case 0xB8:
+            res(7, BC.high_8);
+            return 2;
+        case 0xB9:
+            res(7, BC.low_8);
+            return 2;
+        case 0xBA:
+            res(7, DE.high_8);
+            return 2;
+        case 0xBB:
+            res(7, DE.low_8);
+            return 2;
+        case 0xBC:
+            res(7, HL.high_8);
+            return 2;
+        case 0xBD:
+            res(7, HL.low_8);
+            return 2;
+            //TODO 0XBE
+        case 0xBF:
+            res(7, A.high_8);
+            return 2;
+
+        case 0xC0:
+            set(0, BC.high_8);
+            return 2;
+        case 0xC1:
+            set(0, BC.low_8);
+            return 2;
+        case 0xC2:
+            set(0, DE.high_8);
+            return 2;
+        case 0xC3:
+            set(0, DE.low_8);
+            return 2;
+        case 0xC4:
+            set(0, HL.high_8);
+            return 2;
+        case 0xC5:
+            set(0, HL.low_8);
+            return 2;
+            //TODO 0xC6
+        case 0xC7:
+            set(0, A.high_8);
+            return 2;
+        case 0xC8:
+            set(1, BC.high_8);
+            return 2;
+        case 0xC9:
+            set(1, BC.low_8);
+            return 2;
+        case 0xCA:
+            set(1, DE.high_8);
+            return 2;
+        case 0xCB:
+            set(1, DE.low_8);
+            return 2;
+        case 0xCC:
+            set(1, HL.high_8);
+            return 2;
+        case 0xCD:
+            set(1, HL.low_8);
+            return 2;
+            //TODO 0XCE
+        case 0xCF:
+            set(1, A.high_8);
+            return 2;
+        case 0xD0:
+            set(2, BC.high_8);
+            return 2;
+        case 0xD1:
+            set(2, BC.low_8);
+            return 2;
+        case 0xD2:
+            set(2, DE.high_8);
+            return 2;
+        case 0xD3:
+            set(2, DE.low_8);
+            return 2;
+        case 0xD4:
+            set(2, HL.high_8);
+            return 2;
+        case 0xD5:
+            set(2, HL.low_8);
+            return 2;
+            //TODO 0xD6
+        case 0xD7:
+            set(2, A.high_8);
+            return 2;
+        case 0xD8:
+            set(3, BC.high_8);
+            return 2;
+        case 0xD9:
+            set(3, BC.low_8);
+            return 2;
+        case 0xDA:
+            set(3, DE.high_8);
+            return 2;
+        case 0xDB:
+            set(3, DE.low_8);
+            return 2;
+        case 0xDC:
+            set(3, HL.high_8);
+            return 2;
+        case 0xDD:
+            set(3, HL.low_8);
+            return 2;
+            //TODO 0XDE
+        case 0xDF:
+            set(3, A.high_8);
+            return 2;
+
+        case 0xE0:
+            set(4, BC.high_8);
+            return 2;
+        case 0xE1:
+            set(4, BC.low_8);
+            return 2;
+        case 0xE2:
+            set(4, DE.high_8);
+            return 2;
+        case 0xE3:
+            set(4, DE.low_8);
+            return 2;
+        case 0xE4:
+            set(4, HL.high_8);
+            return 2;
+        case 0xE5:
+            set(4, HL.low_8);
+            return 2;
+            //TODO 0xE6
+        case 0xE7:
+            set(4, A.high_8);
+            return 2;
+        case 0xE8:
+            set(5, BC.high_8);
+            return 2;
+        case 0xE9:
+            set(5, BC.low_8);
+            return 2;
+        case 0xEA:
+            set(5, DE.high_8);
+            return 2;
+        case 0xEB:
+            set(5, DE.low_8);
+            return 2;
+        case 0xEC:
+            set(5, HL.high_8);
+            return 2;
+        case 0xED:
+            set(5, HL.low_8);
+            return 2;
+            //TODO 0XEE
+        case 0xEF:
+            set(5, A.high_8);
+            return 2;
+        case 0xF0:
+            set(6, BC.high_8);
+            return 2;
+        case 0xF1:
+            set(6, BC.low_8);
+            return 2;
+        case 0xF2:
+            set(6, DE.high_8);
+            return 2;
+        case 0xF3:
+            set(6, DE.low_8);
+            return 2;
+        case 0xF4:
+            set(6, HL.high_8);
+            return 2;
+        case 0xF5:
+            set(6, HL.low_8);
+            return 2;
+            //TODO 0xD6
+        case 0xF7:
+            set(6, A.high_8);
+            return 2;
+        case 0xF8:
+            set(7, BC.high_8);
+            return 2;
+        case 0xF9:
+            set(7, BC.low_8);
+            return 2;
+        case 0xFA:
+            set(7, DE.high_8);
+            return 2;
+        case 0xFB:
+            set(7, DE.low_8);
+            return 2;
+        case 0xFC:
+            set(7, HL.high_8);
+            return 2;
+        case 0xFD:
+            set(7, HL.low_8);
+            return 2;
+            //TODO 0XFE
+        case 0xFF:
+            set(7, A.high_8);
+            return 2;
+
     }
     return 0;
 }
