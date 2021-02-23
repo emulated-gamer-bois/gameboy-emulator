26ce8e580e250d34bca46c23a32ad5678bb8c2c3
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2021-02-23 11:59:40 +0100

    Implement LCD and STAT registers

--- src/gameboy/ppu.cpp
+++ src/gameboy/ppu.cpp
@@ -4,6 +4,32 @@
 
 #include "ppu.h"
 
+#define LCDC_ADDRESS 0xFF40
+#define STAT_ADDRESS 0xFF41
+#define LY_ADDRESS 0xFF44
+
+
 ppu::ppu(std::shared_ptr<MMU> memory) {
     this->memory = memory;
+    this->cyclesSinceLastLine = 0;
+}
+
+void ppu::update(uint16_t cpuCycles) {
+    cyclesSinceLastLine += cpuCycles;
+    while (cyclesSinceLastLine >= THRESHOLD) {
+        cyclesSinceLastLine -= THRESHOLD;
+        processNextLine();
+    }
+}
+
+void ppu::processNextLine() {
+    uint8_t currentScanline = memory->read(LY_ADDRESS);
+    LCDC = memory->read(LCDC_ADDRESS);
+    STAT = memory->read(STAT_ADDRESS);
+
+
+    memory->write(STAT_ADDRESS, STAT);
+    memory->write(LCDC_ADDRESS, LCDC);
+    currentScanline = (currentScanline + 1) % LCD_HEIGHT;
+    memory->write(LY_ADDRESS, currentScanline);
 }
--- src/gameboy/ppu.h
+++ src/gameboy/ppu.h
@@ -6,13 +6,51 @@
 #define LAME_BOY_PPU_H
 
 #include <memory>
+#include <cstdint>
 #include "mmu.h"
 
 class ppu {
     std::shared_ptr<MMU> memory;
+    uint16_t cyclesSinceLastLine;
+    const static uint16_t THRESHOLD = 114; //The time it takes to search for objects, draw the line and HBLANK.
+    const static uint16_t LCD_WIDTH = 160;
+    const static uint16_t LCD_HEIGHT = 144;
 
+    union {
+        struct {
+            unsigned int bgWindowDisplayEnable: 1;
+            unsigned int objectDisplayEnable: 1;
+            unsigned int objectSize : 1;
+            unsigned int tileMapDisplaySelect : 1;
+            unsigned int bgWindowTileDataSelect : 1;
+            unsigned int windowDisplayEnable : 1;
+            unsigned int windowTileMapDisplaySelect : 1;
+            unsigned int lcdDisplayEnable : 1;
+        };
+        uint8_t LCDC;
+    };
+
+    union {
+        struct {
+            unsigned int modeFlag: 2;
+            unsigned int coincidenceFlag: 1;
+            unsigned int hBlankInterruptEnable : 1;
+            unsigned int vBlankInterruptEnable : 1;
+            unsigned int oamBlankInterruptEnable : 1;
+            unsigned int lycEqualsLyInterruptEnable : 1;
+            unsigned int UNUSED : 1;
+        };
+        uint8_t STAT;
+    };
+
+    std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT> bytes;
+
+    void processNextLine();
 public:
-    ppu(std::shared_ptr<MMU> memory);
+    explicit ppu(std::shared_ptr<MMU> memory);
+    void update(uint16_t cpuCycles);
+
+
 };
 
 
