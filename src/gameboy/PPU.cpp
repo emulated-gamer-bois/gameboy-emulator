dba45c7a9939e942e180fedfbe495c1048cedbdd
tehAndrew <andpa_kontakt@live.se>
2021-03-06 19:23:24 +0100

    A little cleaning

--- src/gameboy/PPU.cpp
+++ src/gameboy/PPU.cpp
@@ -7,29 +7,27 @@
 #include <cassert>
 
 PPU::PPU(std::shared_ptr<MMU> memory) {
-    SCY = 0;
-    SCX = 0;
-    LY = 0;
-    LYC = 0;
-    WY = 0;
-    WX = 0;
-
-    BGP = 0;
-    OBP0 = 0;
-    OBP1 = 0;
-
-    DMA = 0;
-
-    STAT = 0;
-    LCDC = 0;
+    this->SCY = 0;
+    this->SCX = 0;
+    this->LY = 0;
+    this->LYC = 0;
+    this->WY = 0;
+    this->WX = 0;
+    this->BGP = 0;
+    this->OBP0 = 0;
+    this->OBP1 = 0;
+    this->DMA = 0;
+    this->STAT = 0;
+    this->LCDC = 0;
 
     this->memory = memory;
     this->accumulatedCycles = 0;
     this->modeFlag = OAM_SEARCH;
-    saveRegisters();
-    frameBuffer.fill(0);
     this->readyToDraw = false;
     this->anyStatConditionLastUpdate = false;
+
+    saveRegisters();
+    frameBuffer.fill(0);
 }
 
 void PPU::update(uint16_t cpuCycles) {
@@ -71,6 +69,7 @@ void PPU::update(uint16_t cpuCycles) {
             }
             if (accumulatedCycles >= OAM_SEARCH_THRESHOLD) {
                 accumulatedCycles -= OAM_SEARCH_THRESHOLD;
+                coincidenceFlag = (LYC == LY); // Set coincidence flag before drawing the scanline.
                 modeFlag = SCANLINE_DRAW;
             }
             break;
@@ -83,6 +82,7 @@ void PPU::update(uint16_t cpuCycles) {
             }
             break;
     }
+
     bool meetsStatConditionsCurrent = meetsStatConditions();
     if (!anyStatConditionLastUpdate) {
         if (meetsStatConditionsCurrent) {
@@ -93,11 +93,42 @@ void PPU::update(uint16_t cpuCycles) {
     saveRegisters();
 }
 
+const std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT>* PPU::getFrameBuffer() const {
+    return &frameBuffer;
+}
+
+bool PPU::isReadyToDraw() const {
+    return this->readyToDraw;
+}
+
+void PPU::confirmDraw() {
+    this->readyToDraw = false;
+}
+
 void PPU::processNextLine() {
     if (bgWindowDisplayEnable) {
         drawBackgroundScanLine();
     }
     //TODO window and object drawing
+}
+
+void PPU::drawBackgroundScanLine() {
+    uint16_t bgMapStartAddress;
+    if (tileMapDisplaySelect) {
+        bgMapStartAddress = BG_WINDOW_MAP1;
+    } else {
+        bgMapStartAddress = BG_WINDOW_MAP0;
+    }
+
+    //For each pixel in the current row, find the correct tile ID, then the
+    //correct pixel in that tile
+    for (uint8_t x = 0; x < 160; ++x) {
+        uint8_t absolutePixelX = (SCX + x) % 256;
+        uint8_t absolutePixelY = (SCY + LY) % 256;
+        uint8_t tileID = getTileID(bgMapStartAddress, absolutePixelX, absolutePixelY);
+        uint8_t pixel = getTilePixelColor(tileID, absolutePixelX, absolutePixelY);
+        frameBuffer[LY * 160 + x] = pixel;
+    }
 }
 
 void PPU::initRegisters() {
@@ -122,25 +153,6 @@ void PPU::saveRegisters() { //TODO check if anything else needs doing here
     memory->write(STAT_ADDRESS, STAT);
 
     memory->write(LY_ADDRESS, LY);
-}
-
-void PPU::drawBackgroundScanLine() {
-    uint16_t bgMapStartAddress;
-    if (tileMapDisplaySelect) {
-        bgMapStartAddress = BG_WINDOW_MAP1;
-    } else {
-        bgMapStartAddress = BG_WINDOW_MAP0;
-    }
-
-    //For each pixel in the current row, find the correct tile ID, then the
-    //correct pixel in that tile
-    for (uint8_t x = 0; x < 160; ++x) {
-        uint8_t absolutePixelX = (SCX + x) % 256;
-        uint8_t absolutePixelY = (SCY + LY) % 256;
-        uint8_t tileID = getTileID(bgMapStartAddress, absolutePixelX, absolutePixelY);
-        uint8_t pixel = getTilePixelColor(tileID, absolutePixelX, absolutePixelY);
-        frameBuffer[LY * 160 + x] = pixel;
-    }
 }
 
 /**
@@ -187,29 +199,13 @@ uint8_t PPU::getTilePixelColor(uint8_t tileID, uint8_t absolutePixelX, uint8_t a
     return ((BGP >> (2 * pixelColor)) & bitmask);
 }
 
-std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT>* PPU::getFrameBuffer() {
-    return &frameBuffer;
-}
+bool PPU::meetsStatConditions() const {
+    auto lycInterrupt = (lycEqualsLyInterruptEnable && coincidenceFlag);
+    auto oamInterrupt = (oamInterruptEnable && modeFlag == OAM_SEARCH);
+    auto vBlankInterrupt = (vBlankInterruptEnable && modeFlag == VBLANK);
+    auto hBlankInterrupt = (hBlankInterruptEnable && modeFlag == HBLANK);
 
-uint8_t PPU::getMode() const {
-    return modeFlag;
-}
-
-bool PPU::meetsStatConditions() {
-    if (lycEqualsLyInterruptEnable && LY == LYC) {
-        coincidenceFlag = 1;
-        return true;
-    }
-    if (oamInterruptEnable && modeFlag == OAM_SEARCH) {
-        return true;
-    }
-    if (vBlankInterruptEnable && modeFlag == VBLANK) {
-        return true;
-    }
-    if (hBlankInterruptEnable && modeFlag == HBLANK) {
-        return true;
-    }
-    return false;
+    return (lycInterrupt || oamInterrupt || vBlankInterrupt || hBlankInterrupt);
 }
 
 void PPU::vBlankInterrupt() {
@@ -222,12 +218,4 @@ void PPU::statInterrupt() {
     uint8_t interruptFlags = memory->read(INTERRUPT_FLAG);
     interruptFlags |= (1 << 1);
     memory->write(INTERRUPT_FLAG, interruptFlags);
-}
-
-bool PPU::isReadyToDraw() const {
-    return this->readyToDraw;
-}
-
-void PPU::confirmDraw() {
-    this->readyToDraw = false;
 }
--- src/gameboy/PPU.h
+++ src/gameboy/PPU.h
@@ -6,9 +6,11 @@
 
 #include <memory> // smart pointers
 #include <cstdint> // uint8_t and uint16_t
-#include "MMU.h"
-#include "Definitions.h" // LCD_WIDTH and LCD_HEIGHT
 
+#include "Definitions.h" // LCD_WIDTH and LCD_HEIGHT
+#include "MMU.h"
+
+// Register addresses
 #define LCDC_ADDRESS 0xFF40
 #define STAT_ADDRESS 0xFF41
 #define SCY_ADDRESS 0xFF42
@@ -22,9 +24,11 @@
 #define WY_ADDRESS 0xFF4A
 #define WX_ADDRESS 0xFF4B
 
+// Tilemap memory block modes.
 #define BG_WINDOW_MAP0 0x9800
 #define BG_WINDOW_MAP1 0x9C00
 
+// Tileset memory bock modes.
 #define BG_WINDOW_TILE_DATA0 0x9000
 #define BG_WINDOW_TILE_DATA1 0x8000
 
@@ -34,7 +38,18 @@ friend class test_case_name##_##test_name##_Test
 class PPU {
 public:
     explicit PPU(std::shared_ptr<MMU> memory);
+
     void update(uint16_t cpuCycles);
+
+    const std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT>* getFrameBuffer() const;
+    bool isReadyToDraw() const;
+    void confirmDraw();
+
+private:
+    const static uint16_t HBLANK_THRESHOLD = 51; //The time it takes to search for objects, draw the line and HBLANK.
+    const static uint16_t VBLANK_LINE_THRESHOLD = 114;
+    const static uint16_t OAM_SEARCH_THRESHOLD = 20;
+    const static uint16_t SCANLINE_DRAW_THRESHOLD = 43;
 
     enum Mode {
         HBLANK,
@@ -43,20 +58,7 @@ public:
         SCANLINE_DRAW
     };
 
-    bool isReadyToDraw() const;
-    void confirmDraw();
-    std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT>* getFrameBuffer();
-    uint8_t getMode() const;
-private:
-    const static uint16_t HBLANK_THRESHOLD = 51; //The time it takes to search for objects, draw the line and HBLANK.
-    const static uint16_t VBLANK_LINE_THRESHOLD = 114;
-    const static uint16_t OAM_SEARCH_THRESHOLD = 20;
-    const static uint16_t SCANLINE_DRAW_THRESHOLD = 43;
-
-    std::shared_ptr<MMU> memory;
-    std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT> frameBuffer;
-    uint16_t accumulatedCycles;
-
+    // LCD control register
     union {
         struct { //bits 0-7
             unsigned int bgWindowDisplayEnable: 1;
@@ -71,6 +73,7 @@ private:
         uint8_t LCDC;
     };
 
+    // LCD status register
     union {
         struct {
             unsigned int modeFlag: 2;
@@ -84,30 +87,35 @@ private:
         uint8_t STAT;
     };
 
+    // Misc registers
     uint8_t SCY;
     uint8_t SCX;
     uint8_t LY;
     uint8_t LYC;
     uint8_t WY;
     uint8_t WX;
-
     uint8_t BGP;
     uint8_t OBP0;
     uint8_t OBP1;
-
     uint8_t DMA;
 
-    bool meetsStatConditions();
-    void vBlankInterrupt();
-    void statInterrupt();
+    std::shared_ptr<MMU> memory;
+    std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT> frameBuffer;
+    uint16_t accumulatedCycles;
+    bool readyToDraw;
+    bool anyStatConditionLastUpdate;
 
     void processNextLine();
-    void initRegisters();
     void drawBackgroundScanLine();
+    void initRegisters();
     void saveRegisters();
+
+    // MMU reading functions.
     uint8_t getTileID(uint16_t bgMapStart, uint8_t pixelAbsoluteX, uint8_t pixelAbsoluteY);
     uint8_t getTilePixelColor(uint8_t id, uint8_t x, uint8_t y);
 
-    bool readyToDraw;
-    bool anyStatConditionLastUpdate;
+    // Interrupt related functions.
+    bool meetsStatConditions() const;
+    void vBlankInterrupt();
+    void statInterrupt();
 };
