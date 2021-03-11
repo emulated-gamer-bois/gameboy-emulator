be5e2aa55367920626b8c39f78be710e0173fcbf
Algot Axelzon <algot.axelzon@gmail.com>
2021-03-11 14:11:18 +0100

    Disable window and sprites in boot ROM

--- roms/asm/boot_lameboy_big.asm
+++ roms/asm/boot_lameboy_big.asm
@@ -6,8 +6,9 @@ Start:
     ld sp, $fffe
 
     ;Set LCDC to use tile map 0 and 8000 addressing mode
+    ;Window disabled, Sprites disabled
     ld hl, $ff40
-    ld [hl], $b3
+    ld [hl], %10010001
 
     ;Set pallet so that 0=white, 1=light grey, 2=dark grey, 3=black
     ld hl, $ff47
