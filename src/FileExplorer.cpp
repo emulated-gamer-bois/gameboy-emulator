461df33b4ff62f6b7d409c58a4b8b78557772a5c
tehAndrew <andpa_kontakt@live.se>
2021-03-24 21:17:34 +0100

    Changed filesystem design

---
+++ src/FileExplorer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andreas on 2021-03-24.
+//
+
+#include "FileExplorer.h"
---
+++ src/FileExplorer.h
@@ -0,0 +1,16 @@
+//
+// Created by Andreas on 2021-03-24.
+//
+
+#ifndef LAME_BOY_FILEEXPLORER_H
+#define LAME_BOY_FILEEXPLORER_H
+
+
+
+class FileExplorer {
+
+};
+
+
+
+#endif //LAME_BOY_FILEEXPLORER_H
