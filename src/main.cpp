6f4bced03b0360a83a90e30d1af707398143b88f
David Möller <davidm@student.chalmers.se>
2021-05-05 14:00:01 +0200

    Fixed grammar

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]) {
 #ifdef WIN32
-    //Hides console window if the project is build on Windows
+    //Hides console window if the project is built on Windows
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 #endif
     Application app;
